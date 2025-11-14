44366f88fd6723a20ea7e8c0cf8599832207f4be
PriyankaKamble <priyankakamble18042003@gmail.com>
2025-11-14 18:01:52 +0530

    paramcon

---
+++ SquareApp.java
@@ -0,0 +1,14 @@
+class Square
+{
+	Square(int x)
+	{
+	System.out.printf("Square is %d\n",x*x);
+	}
+}
+public class SquareApp
+{
+	public static void main(String x[])
+	{
+		Square s=new Square(5);
+	}
+}
